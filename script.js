bc2280dfb1950b0caf341459d8a93220d5c5a504
S K Sahil Mandal <91855497+shekhsahil@users.noreply.github.com>
2023-09-22 14:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// script.js
+const categorySelect = document.getElementById("categorySelect");
+const generateButton = document.getElementById("generateButton");
+const quoteText = document.getElementById("quoteText");
+const authorText = document.getElementById("authorText");
+const favoriteButton = document.getElementById("favoriteButton");
+const favoritesList = document.getElementById("favoritesList");
+const copyButton = document.getElementById("copyButton");
+
+let favoriteQuotes = [];
+
+generateButton.addEventListener("click", generateQuote);
+favoriteButton.addEventListener("click", addToFavorites);
+copyButton.addEventListener("click", copyFavorites);
+categorySelect.addEventListener("change", generateQuote);
+
+function generateQuote() {
+    const selectedCategory = categorySelect.value;
+    let apiUrl = "https://api.quotable.io/random";
+
+    if (selectedCategory) {
+        apiUrl += `?tags=${selectedCategory}`;
+    }
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            quoteText.textContent = data.content;
+            authorText.textContent = `- ${data.author}`;
+        })
+        .catch(error => console.error(error));
+}
+
+function addToFavorites() {
+    const currentQuote = `${quoteText.textContent} - ${authorText.textContent}`;
+    if (currentQuote && !favoriteQuotes.includes(currentQuote)) {
+        favoriteQuotes.push(currentQuote);
+        const listItem = document.createElement("li");
+        listItem.textContent = currentQuote;
+        favoritesList.appendChild(listItem);
+    }
+}
+
+function copyFavorites() {
+    const textToCopy = favoriteQuotes.join("\n");
+    if (textToCopy) {
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                alert("Favorites copied to clipboard!");
+            })
+            .catch(error => console.error(error));
+    }
+}
